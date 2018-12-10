eac3726f79073b76f75db449a1e107bcc3e14a39
vinaykashyap1996 <vinay.kashyap@accionlabs.com>
2018-12-10 19:21:07 +0530

    first commit

--- routes/card.js
+++ routes/card.js
@@ -3,6 +3,7 @@ let router = express.Router();
 module.exports = router;
 
 let Api_Card=require('../controllers/card');
+let Auth1=require('../middleware/check-auth');
 
 router.post('/card',Api_Card.card);
-router.get('/card/:id',Api_Card.Card_Get);
+router.get('/card/:id',Auth1,Api_Card.Card_Get);
